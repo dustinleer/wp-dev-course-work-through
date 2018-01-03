8199ff73643ad42c7c29408d47ad6cc0b71bc081
Dustin Leer <dustin@wildcanon.com>
2018-01-03 11:03:14 -0500

    Adds proper indentation

--- 3 - Template Hierarchy/3.40 - finished files/wphierarchy/index.php
+++ 3 - Template Hierarchy/3.40 - finished files/wphierarchy/index.php
@@ -6,21 +6,21 @@
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
 			
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				
-				<header class="entry-header">
-				
-					<?php the_title( '<h1>', '</h1>' ); ?>
-
-				</header>
-
-				<div class="entry-content">
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					
-					<?php the_content(); ?>
-				
-				</div>
+					<header class="entry-header">
+					
+						<?php the_title( '<h1>', '</h1>' ); ?>
 
-			</article>
+					</header>
+
+					<div class="entry-content">
+						
+						<?php the_content(); ?>
+					
+					</div>
+
+				</article>
 
 			<?php endwhile; else: ?>
 
