e2bd8ac58faa89e2ee40a626a5c39263b95e8667
Dustin Leer <dustin@wildcanon.com>
2018-01-03 11:16:42 -0500

    Creates template parts for modularity

---
+++ 3 - Template Hierarchy/3.40 - finished files/wphierarchy/template-parts/content-none.php
@@ -0,0 +1,15 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	<header class="entry-header">
+	
+		<h1><?php esc_html_e( '404', 'wp_hierarchy' ) ?></h1>
+
+	</header>
+
+	<div class="entry-content">
+		
+		<p><?php esc_html_e( 'Sorry! No content found.', 'wp_hierarchy' ) ?></p>
+	
+	</div>
+
+</article>
