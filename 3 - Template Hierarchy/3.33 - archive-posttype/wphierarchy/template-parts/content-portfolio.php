95438305222a05915881ba2bed5e065bbf2d52f7
Dustin Leer <dustin@wildcanon.com>
2018-08-13 16:55:09 -0400

    Works through Single Pagse and Archives

--- 3 - Template Hierarchy/3.33 - archive-posttype/wphierarchy/template-parts/content-portfolio.php
+++ 3 - Template Hierarchy/3.33 - archive-posttype/wphierarchy/template-parts/content-portfolio.php
@@ -3,11 +3,11 @@
   <header class="entry-header">
 
     <?php the_title( '<h2><a href="' . get_the_permalink() .'">', '</a></h2>' ); ?>
-
+    
     <a href="<?php the_permalink(); ?>">
-      <?php the_post_thumbnail( 'full' ); ?>
+		<?php the_post_thumbnail( 'full' ); ?>
     </a>
-
+    	
   </header>
 
   <div class="entry-content">
